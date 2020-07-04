3b921b2f2321e2b673c3414df4bc33955fbb7d89
Diegod96 <ddphillyfan@gmail.com>
2020-07-03 22:41:01 -0400

    Able to stream data via Firehose to S3

--- create_stream.py
+++ create_stream.py
@@ -1,43 +1,79 @@
+from tweepy import Stream, OAuthHandler
+from tweepy.streaming import StreamListener
+import json
 import boto3
-import time
-import sys
-from moto import mock_kinesis
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from unidecode import unidecode
 from config import *
-from botocore.exceptions import ClientError
 
+
+# Instantiate vaderSentiment analyzer
+analyzer = SentimentIntensityAnalyzer()
+
+# Credentials for Twitter & AWS
+consumer_key = ckey
+consumer_secret = csecret
+access_token = atoken
+access_token_secret = atokensecret
 aws_key_id = accesskeyid
 aws_key = secretaccesskey
 
 
-def create_stream(client, stream_name):
-    return client.create_delivery_stream(
-        DeliveryStreamName=stream_name,
-        S3DestinationConfigureation={
-            'RoleARN': 'arn:aws:iam::743362587039:role/service-role/KinesisFirehoseServiceRole-diego-tweets--us-east-1-1593268563236',
-            'BucketARN': 'arn:aws:s3:::diego-twitter-stream-sink',
-            'Prefix': stream_name + '//'
-        }
-    )
+def sentiment_analysis(text):
+    """
+    Performs sentiment analysis on tweet text
+    :param text:
+    :return: sentiment
+    """
+    vs = analyzer.polarity_scores(text)
+    sentiment = str(vs['compound'])
+    return sentiment
 
 
-def main(search_name):
-    stream_name = search_name[0]
-    client = boto3.client('firehose', region_name="us-east-1",
-                          aws_access_key_id=aws_key_id,
-                          aws_secret_access_key=aws_key
-                          )
+class TweetStreamListener(StreamListener):
 
-    try:
-        create_stream(client, stream_name)
-        print("Creating Kinesis Firehose stream...")
-        time.sleep(60)
-    except:
-        pass
+    def on_data(self, data):
+        """
+        Picks out english language tweets and parses them up into a dictionary of the tweet's timestamp, text,
+        and sentiment score
+        :param data:
+        :return:
+        """
+        all_data = json.loads(data)
+        tweet_data = {}
+        try:
+            if 'lang' in all_data and (all_data['lang'] == "en"):
+                tweet_data['ts'] = str(all_data["timestamp_ms"])
+                # tweet_data['status_id'] = str(all_data["id"])
+                tweet_data['text'] = unidecode(all_data["text"])
+                tweet_data['sentiment'] = str(sentiment_analysis(all_data["text"]))
+                kinesis_client.put_record(
+                    DeliveryStreamName=stream_name,
+                    Record={
+                        'Data': json.dumps(tweet_data) + '\n'
+                    }
+                )
+                print(tweet_data)
+        except (AttributeError, Exception) as e:
+            print(e)
+        return True
 
-    stream_status = client.describe_delivery_stream(DeliveryStreamName=stream_name)
-    if stream_status['DeliveryStreamDescription']['DeliveryStreamStatus'] == 'ACTIVE':
-        print("\n ==== KINESES ONLINE ====")
+    def on_error(self, status):
+        """
+        Return an status code error
+        :param status:
+        :return:
+        """
+        print(status)
 
 
 if __name__ == '__main__':
-    main("trump")
+    kinesis_client = boto3.client('firehose',
+                                  region_name=region,  # enter the region
+                                  aws_access_key_id=accesskeyid,  # fill your AWS access key id
+                                  aws_secret_access_key=secretaccesskey)  # fill you aws secret access key
+    listener = TweetStreamListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, listener)
+    stream.filter(track=["a", "e", "i", "o", "u"])
--- delete_stream.py
+++ delete_stream.py
@@ -9,6 +9,12 @@ aws_key = secretaccesskey
 
 
 def delete_stream(client, stream_name):
+    """
+    Deletes Kinesis Firehose stream
+    :param client:
+    :param stream_name:
+    :return:
+    """
     try:
         print("Successfully delete kinesis stream: {}".format(stream_name))
         return client.delete_delivery_stream(DeliveryStreamName=stream_name)
@@ -17,6 +23,11 @@ def delete_stream(client, stream_name):
 
 
 def main(stream_name):
+    """
+    Connects to Kinesis Firehose
+    :param stream_name:
+    :return:
+    """
     client = boto3.client("firehose", region_name="us-east-1",
                           aws_access_key_id=aws_key_id,
                           aws_secret_access_key=aws_key
