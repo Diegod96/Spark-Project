dc10362b92d3612bdbd40796a94a18d1f36d9638
Diegod96 <ddphillyfan@gmail.com>
2020-07-13 14:57:36 -0400

    Forgot to convert a config var

--- create_stream.py
+++ create_stream.py
@@ -68,7 +68,7 @@ class TweetStreamListener(StreamListener):
                           (tweet_data['ts'], tweet_data['text'], tweet_data['sentiment']))
                 conn.commit()
                 kinesis_client.put_record(
-                    DeliveryStreamName=stream_name,
+                    DeliveryStreamName=os.environ.get("stream_name"),
                     Record={
                         'Data': json.dumps(tweet_data) + '\n'
                     }
